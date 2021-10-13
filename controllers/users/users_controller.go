07ae84fa3c8137c718039a0a7db18ed8aa6c23a2
Wilker Alves <wilkeralves97@gmail.com>
2021-10-13 15:55:21 -0300

    starting from domain

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,27 @@
 package users
 
 import (
+	"github.com/WilkerAlves/bookstore_users-api/domain/users"
+	"github.com/WilkerAlves/bookstore_users-api/services"
+	"github.com/WilkerAlves/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadResquestError("invalid json body")
+		c.JSON(restErr.Status, restErr.Message)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr.Message)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- domain/users/user.go
+++ domain/users/user.go
@@ -1,1 +1,9 @@
 package users
+
+type User struct {
+	Id          int64  `json:"id"`
+	FirstName   string `json:"first_name" validate:"required,min=4,max=15"`
+	LastName    string `json:"last_name" validate:"required,min=4,max=15"`
+	Email       string `json:"email" validate:"required,email"`
+	DateCreated string `json:"date_created"`
+}
